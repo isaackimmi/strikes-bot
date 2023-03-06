e7a403807b4df0b2bb352c910889aa9857e7d4b8
Isaac Kim <isaacmmikim@gmail.com>
2023-03-05 21:15:54 -0800

    display all strikes partially working.

---
+++ commands/getAllStrikes.js
@@ -0,0 +1,53 @@
+import Lyra from "@lyrafinance/lyra-js";
+import moment from "moment";
+
+const lyra = new Lyra();
+
+export const getAllStrikes = async (underlying, expiry) => {
+  switch (underlying) {
+    case "ETH":
+      underlying = "sETH-sUSD";
+      break;
+    case "BTC":
+      underlying = "sBTC-sUSD";
+      break;
+    case "ETH":
+      underlying = "sSOL-sUSD";
+      break;
+    default:
+      break;
+  }
+  //  console.log(lyra.markets());
+  //  console.log(underlying);
+  //  console.log(moment(expiry).unix());
+
+  expiry = moment(expiry).unix();
+  const markets = await lyra.markets();
+
+  const market = markets.find(
+    (market) => market.name.toLowerCase() === underlying.toLowerCase()
+  );
+  if (!market) {
+    return "Invalid underlying";
+  }
+
+  const board = market
+    .liveBoards()
+    .find((board) => board.expiryTimestamp === expiry);
+  if (!board) {
+    return "Invalid expiry";
+  }
+
+  console.log(board);
+
+  console.log(board);
+  const strikes = board.strikes().map((strike) => ({
+    id: strike.id,
+    strikePrice: strike.strikePrice,
+    //callLiquidity: strike.call.liquidity(),
+    //putLiquidity: strike.put.liquidity(),
+  }));
+
+  console.log(strikes);
+  return strikes;
+};
--- index.js
+++ index.js
@@ -1,22 +1,72 @@
 import Discord from "discord.js";
-import config from "./config.js";
+import Lyra from "@lyrafinance/lyra-js";
 import { ping } from "./commands/ping.js";
+import { getAllStrikes } from "./commands/getAllStrikes.js";
+import config from "./config.js";
+
+const lyra = new Lyra();
 
 const client = new Discord.Client();
+
+async function run() {
+  const markets = await lyra.markets();
+
+  //console.log(
+  //  markets[0].liveBoards().map((board) => ({
+  //    id: board.id,
+  //    expiryTimestamp: board.expiryTimestamp,
+  //    strikes: board.strikes().map((strike) => ({
+  //      id: strike.id,
+  //      strikePrice: strike.strikePrice,
+  //    })),
+  //  }))
+  //  //markets.map((market) => ({
+  //  //  address: market.address,
+  //  //  name: market.name,
+  //  //  expiries: market.liveBoards().map((board) => ({
+  //  //    id: board.id,
+  //  //    expiryTimestamp: board.expiryTimestamp,
+  //  //    strikes: board.strikes().map((strike) => ({
+  //  //      id: strike.id,
+  //  //      strikePrice: strike.strikePrice,
+  //  //    })),
+  //  //  })),
+  //  //}))
+  //);
+}
 
 // Event listeners
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
+  run();
 });
 
-client.on("message", (message) => {
+client.on("message", async (message) => {
   if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
   const args = message.content.slice(config.prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if (command === "ping") {
-    ping.execute(client, message, args);
+  //if (command === "ping") {
+  //  ping.execute(client, message, args);
+  //}
+
+  if (command === "getallstrikes") {
+    const [underlying, expiry] = args;
+    const strikes = await getAllStrikes(underlying, expiry);
+
+    console.log(strikes);
+    if (typeof strikes === "string") {
+      message.reply(strikes);
+    } else {
+      const formattedStrikes = strikes
+        .map(
+          (strike) =>
+            `${strike.strikePrice}: Call Liquidity = ${strike.callLiquidity}, Put Liquidity = ${strike.putLiquidity}`
+        )
+        .join("\n");
+      message.reply(`Strikes:\n${formattedStrikes}`);
+    }
   }
 });
 
