d7528190288fc9d2d154af884142bc6db230722a
Isaac Kim <isaacmmikim@gmail.com>
2023-05-30 18:01:57 -0700

    display expiries in AEST.

--- utils/fromUnixEpoch.js
+++ utils/fromUnixEpoch.js
@@ -1,27 +1,30 @@
 export const fromUnixEpoch = (timestamp) => {
   const date = new Date(timestamp * 1000);
-  const monthNames = [
-    "Jan",
-    "Feb",
-    "Mar",
-    "Apr",
-    "May",
-    "Jun",
-    "Jul",
-    "Aug",
-    "Sep",
-    "Oct",
-    "Nov",
-    "Dec",
-  ];
-  const month = monthNames[date.getMonth()];
-  const day = date.getDate();
-  const hour = date.getHours();
-  const ampm = hour >= 12 ? "PM" : "AM";
-  const hour12 = hour % 12 || 12;
 
-  return `${month} ${day}, ${hour12}:${String(date.getMinutes()).padStart(
-    2,
-    "0"
-  )}${ampm}`;
+  const options = {
+    timeZone: "Australia/Sydney",
+    hour12: true,
+    hour: "numeric",
+    minute: "numeric",
+    day: "numeric",
+    month: "short",
+  };
+
+  let formattedDate = new Intl.DateTimeFormat("en-US", options).format(date);
+
+  // The formatted date might look like "Apr 15, 2:45 PM"
+  // Extract month, day, hour, minute and am/pm parts using regex
+  let match = formattedDate.match(/(\w+)\s(\d+),\s(\d+):(\d+)\s(\w+)/);
+
+  if (!match) {
+    // Couldn't match the expected format, return original date string
+    return date.toString();
+  }
+
+  let [_, month, day, hour, minute, ampm] = match;
+
+  // Make sure day and month don't have leading zeros
+  day = day.startsWith("0") ? day.slice(1) : day;
+
+  return `${month} ${day}, ${hour}:${minute}${ampm.toUpperCase()}`;
 };
